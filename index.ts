28f891460c841265928c30ee942df3c71b06875d
Derek Anderson <dmikey@users.noreply.github.com>
2023-02-05 04:17:07 +0000

    return json uInt8

--- index.ts
+++ index.ts
@@ -1,7 +1,19 @@
-export function createPDB(numElements: i32, difference: i32): Int8Array {
-  let pdb = new Int8Array(numElements);
-  for (let i: i32 = 0; i < numElements; i++) {
-    pdb[i] = (difference * i) % numElements;
+import { JSON, JSONEncoder } from "assemblyscript-json/assembly";
+
+export function createPDB(numElements: i32, difference: i32): Uint8Array[] {
+  let pdb = new Array<Uint8Array>(numElements);
+  // Create encoder
+
+  for (let i = 0; i < numElements; i++) {
+    // Construct necessary object
+    let encoder = new JSONEncoder();
+    encoder.pushObject("obj");
+    encoder.setInteger("int", 10);
+    encoder.setString("str", `${(difference * i) % numElements}`);
+    encoder.popObject();
+
+    let json: Uint8Array = encoder.serialize();
+    pdb[i] = json;
   }
   return pdb;
 }
